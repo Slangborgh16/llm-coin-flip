59e4ece1ea6b6e428bf561b1f9f7ffb9c994ee02
Sam Langborgh <slangborgh16@gmail.com>
2024-01-24 19:30:24 -0500

    Added support for command line arguments

--- coinflip.py
+++ coinflip.py
@@ -21,16 +21,30 @@ def flip_a_coin(model: str='gpt-3.5-turbo', temperature: float=1.5) -> str:
     return response.choices[0].message.content
 
 
-def main(trials: int, model: str, temperature: float) -> None:
-    for trial in range(1, trials + 1):
-        print(f'Trial {trial}')
-        result: str = flip_a_coin(model, temperature)
-        print(f'\t{result}')
-        time.sleep(5)
-
-
 if __name__ == '__main__':
-    trials: int = 10
-    model: str = 'gpt-3.5-turbo'
-    temperature: float = 1.5
-    main(trials, model, temperature)
+    description: str = 'A slightly overkill way to flip a coin using GPT'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('trials', default=1, type=int, nargs='?', \
+            help='Number of trials to run. Default: 1')
+    parser.add_argument('-m', '--model', default='gpt-3.5-turbo', type=str, \
+            help='GPT model to use. Default: gpt-3.5-turbo')
+    parser.add_argument('-t', '--temperature', default=1.5, type=float, \
+            help='LLM temperature. Default: 1.5')
+    parser.add_argument('-p', '--plot', action='store_true', \
+            help='Display a live-updating plot of the results for more than 1 trial.')
+    args = parser.parse_args()
+
+    trials: int = args.trials
+    model: str = args.model
+    temperature = args.temperature
+    show_plot: bool = args.plot
+
+    if trials > 1:
+        for trial in range(1, trials + 1):
+            print(f'Trial {trial}')
+            result: str = flip_a_coin(model, temperature)
+            print(f'\t{result}')
+            time.sleep(5)
+    else:
+        result: str = flip_a_coin(model, temperature)
+        print(result)
